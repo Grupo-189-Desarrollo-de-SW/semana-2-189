8c6717dbcd3ebe0dd746fa08e99be3ba42fd153e
Gustavo García <gustavogarcia@utp.edu.co>
2020-12-04 10:36:37 -0500

    Add files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+
+    <!-- Header -->
+
+    <!-- Fin del Header -->
+
+    <!-- Seccion API -->
+
+    <!-- Fin seccion API -->
+
+    <!-- Sección de Equipo -->
+
+    <!-- Fin sección Equipo -->
+
+    <!-- Sección Footer -->
+
+    <!-- Fin sección Footer -->
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
