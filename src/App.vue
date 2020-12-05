f2218a2fbf3db5629d4455d02fda8725a1f7b822
Gustavo García <gustavogarcia@utp.edu.co>
2020-12-04 23:24:25 -0500

    Cambios importantes referentes a la distribucion  de codigo entre App.vue y el componente APICard, no se agregaron funcionalidades

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-
     <!-- Header -->
 
     <!-- Fin del Header -->
@@ -8,7 +7,18 @@
     <!-- Seccion API -->
 
     <!-- Fin seccion API -->
-    <APICard></APICard>
+    <div class="container-fluid my-4 container-API" id="news">
+      <h2 style="color: white;text-align:center;">Peliculas</h2>
+      <hr width="100%" />
+      <div class="row mt-md-5 mt-sm-5 mt-xs-5">
+        <div class="col-lg-6 col-xs-12 container-movie"
+          v-for="item in moviesFull"
+          :key="item.id"
+        >
+          <APICard :movie="item"></APICard>
+        </div>
+      </div>
+    </div>
     <!-- Sección de Equipo -->
 
     <!-- Fin sección Equipo -->
@@ -16,19 +26,74 @@
     <!-- Sección Footer -->
 
     <!-- Fin sección Footer -->
-
   </div>
 </template>
 
 <script>
-
-import APICard from "./components/APICard.vue"
+import axios from "axios";
+import APICard from "./components/APICard.vue";
 
 export default {
-  name: 'App',
-  components:{
-    APICard
-    }
-}
+  name: "App",
+  data() {
+    return {
+      moviesFull: [],
+    };
+  },
+  components: {
+    APICard,
+  },
+  methods: {
+    extraerPeliculas() {
+      const apiKey = "39dddcd1";
+      const search = "sea";
+      axios
+        .get(`http://www.omdbapi.com/?s=${search}&type=movie&apikey=${apiKey}`)
+        .then((response) => response.data.Search.splice(1, 4))
+        .then((movies) => {
+          movies.forEach((movie) => {
+            axios
+              .get(
+                `http://www.omdbapi.com/?i=${movie.imdbID}&t=${movie.Title}&type=movie&y=${movie.Year}&plot=short&apikey=${apiKey}`
+              )
+              .then((response) => {
+                let moviedata = response.data;
+                moviedata.Pageimdb = `https://www.imdb.com/title/${moviedata.imdbID}/`;
+                moviedata.imdbRating = String(
+                  parseFloat(moviedata.imdbRating) * 10
+                );
+                this.moviesFull.push(moviedata);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          });
+        });
+    },
+  },
+  created() {
+    this.extraerPeliculas();
+  },
+};
 </script>
 
+<style scoped>
+.container-API {
+  background-color: #27057d;
+  box-shadow: 4px 4px 4px 4px rgba(39, 5, 125, 0.2);
+}
+hr {
+  height: 1px;
+  color: white;
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  padding-top: 0px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+}
+.container-movie {
+  background-color: #ededed;
+  border: 10px solid #27057d;
+  border-radius: 20px;
+}
+</style>
