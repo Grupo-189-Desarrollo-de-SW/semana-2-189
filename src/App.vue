dba7fbacd1201fc07d273a7149c9e74a1bdbdf75
alfredgalindez <alfredgalindez@gmail.com>
2020-12-06 00:30:01 -0500

    Seccion Team funcional por mejorar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-
+    
     <!-- Header -->
 
     <!-- Fin del Header -->
@@ -10,7 +10,13 @@
     <!-- Fin seccion API -->
 
     <!-- Sección de Equipo -->
-
+    <div class="container-fluid">
+    <div class="row justify-content-center mb-5">
+      <div class="col mt-5" v-for="(item, index) of team" :key="index">
+        <team-card :member="item"></team-card>
+      </div>
+    </div>
+  </div>
     <!-- Fin sección Equipo -->
 
     <!-- Sección Footer -->
@@ -21,9 +27,59 @@
 </template>
 
 <script>
+import TeamCard from './components/TeamCard.vue'
 
 export default {
   name: 'App',
+  components: {
+    TeamCard
+  },
+  data(){
+    return{
+      team : [
+        {
+          codigo : 1,
+          nombre: "Juan Jose Neira Cote",
+          descripcion: 
+            " Hace parte de Mision TIC 2022, y se encuentra en desarrollo del ciclo 3 - Desarrollo de sotfware",
+          rol : "Empíric WE",
+          imagen: "images/Juan.jpg",
+        },
+        {
+          codigo : 2,
+          nombre: "Gustavo Adolfo Garcia Londoño",
+          descripcion: 
+            " Hace parte de Mision TIC 2022, y se encuentra en desarrollo del ciclo 3 - Desarrollo de sotfware",
+          rol : "Ingeniero de Sistemas",
+          imagen: "images/Gustavo.jpg",
+        },
+        {
+          codigo : 3,
+          nombre: "David Esteban Hernández Garzón",
+          descripcion: 
+            " Hace parte de Mision TIC 2022, y se encuentra en desarrollo del ciclo 3 - Desarrollo de sotfware",
+          rol : "Ingeniero de Sonido",
+          imagen: "images/David.jpg",
+        },
+        {
+          codigo : 4,
+          nombre: "Carlos Alfredo Galindez Muñoz",
+          descripcion: 
+            " Hace parte de Mision TIC 2022, y se encuentra en desarrollo del ciclo 3 - Desarrollo de sotfware",
+          rol : "Ingeniero de Procesos",
+          imagen: "images/Carlos_Galindez.jpg",
+        },
+        {
+          codigo : 5,
+          nombre: "Carlos Gutiérrez",
+          descripcion: 
+            " Hace parte de Mision TIC 2022, y se encuentra en desarrollo del ciclo 3 - Desarrollo de sotfware",
+          rol : "Ingeniero de Procesos",
+          imagen: "images/Carlos_Gutierrez.jpg",
+        },
+      ]
+    }
+  }
 }
 </script>
 
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,7 +1,21 @@
 <template>
-    <div>
-
+            
+    <div class="card text-white bg-dark">
+        <div class="d-flex justify-content-center p-2">
+            <img :src="member.imagen" alt="Foto del Integrante">
+        </div>
+         <div class="card-body">
+            <h4 class="card-title">{{ member.nombre }}</h4>
+            <p class="card-text">{{ member.descripcion}}</p>
+            <p class="card-text"> <span>Rol: </span>{{ member.rol}}</p>
+            <p class="card-text"> <span>Codigo: </span>{{ member.codigo}}</p>
+         </div>
+        <div class="card-footer">
+           <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
     </div>
+        
+    
 </template>
 
 <script>
