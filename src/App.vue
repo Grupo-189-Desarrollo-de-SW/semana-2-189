2289e13c44ad3b17055ee50119b0390c9cc44005
JuanNeiraCote <juanjmeira@gmail.com>
2020-12-05 23:57:13 -0500

    Hola Compañeros, a continuación, cargo issue3 referente al equipo

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,15 @@
     <!-- Fin seccion API -->
 
     <!-- Sección de Equipo -->
-
+         <div class="container-fluid container-miembros">
+      <h2 style="color: white;text-align:center;">Equipo</h2>
+      <hr width="100%" />
+      <div class="row justify-content-center mb-5">
+        <div class="col mt-5 ancho-minimo" v-for="item in members" :key="item.id">
+          <team-card :member="item"></team-card>
+        </div>
+      </div>
+    </div>
     <!-- Fin sección Equipo -->
 
     <!-- Sección Footer -->
@@ -21,9 +29,51 @@
 </template>
 
 <script>
+import TeamCard from './components/TeamCard.vue'
 
 export default {
   name: 'App',
+  components: {TeamCard,
+  },
+  data(){
+    return {
+      members: [{
+        codigo : 1 ,
+        nombre : 'Gustavo García L.' ,
+        descripcion : 'Ingeniero de sistemas, desarrollador Full Stack, Laravel, Node, Git, JS, JAVA.' ,
+        rol : 'Desarrollador FullStack' ,
+        image : require('../public/images/Gustavo.jpg')
+      },
+      {
+        codigo : 2 ,
+        nombre : 'Carlos Galindez' ,
+        descripcion : 'Analista de interfaz de usuario, UI, UX, Bootstrap.' ,
+        rol : 'Desarrollador Frontend' ,
+        image : require('../public/images/Carlos.png')
+      },
+      {
+        codigo : 3 ,
+        nombre : 'Juan J Neira' ,
+        descripcion : 'Data Analyst, css, js, html, php, py developer.' ,
+        rol : 'Desarrollador backend' ,
+        image : require('../public/images/juanneira.jpg')
+      },
+      {
+        codigo : 4 ,
+        nombre : 'David Hernández' ,
+        descripcion : 'Business Consultant, success projector, finance advisor.' ,
+        rol : 'Desarrollador de Negocios' ,
+        image : require('../public/images/fotodave.jpg')
+      },
+      {
+        codigo : 5 ,
+        nombre : 'Carlos Gutierrez' ,
+        descripcion : 'Wireframe planner, UX, UI designer.' ,
+        rol : 'Desarrollador Frontend' ,
+        image : require('../public/images/carlos-gutierrez.jpg')
+      }]
+      }
+    }
 }
 </script>
 
