f9ba38a96b3e09a63cccbe566b738b9607780c21
Gustavo Adolfo Garcia <32319990+gustavogarciautp@users.noreply.github.com>
2020-12-06 01:27:36 -0500

    Ahora se redenderiza el código de cada miembro

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -4,6 +4,7 @@
             <div class="justify-content-center p-2 text-white">
             <img width="100%" height="auto" :src="member.image"/>
             <div class="card-body">
+                <h5 class="card-title text-center">{{member.codigo}}</h5>
                 <h5 class="card-title">{{member.nombre}}</h5>
                 <h6 class="card-title">{{member.rol}}</h6>
                 <p class="card-text">{{member.descripcion}}</p>
