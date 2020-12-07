c22d1f9fcf1b0d70b4f7e55f1d27bf9655603280
daveProgramming98 <72957772+daveProgramming98@users.noreply.github.com>
2020-12-06 20:18:44 -0500

    Update PageHeader.vue

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -26,7 +26,7 @@
               <a class="nav-link" href="#services">Servicios</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#news">Noticias</a>
+              <a class="nav-link" href="#news">Peliculas</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#team">Miembros</a>
